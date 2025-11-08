c839427eb3e9a59698606d39b65b0963fd9c2592
mfchan1 <mfchan@ualberta.ca>
2025-11-08 10:55:59 -0700

    add return button to configure screen

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -1,26 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import LiveGraph from './components/LiveGraph'
-import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
-import Configure from './pages/Configuration'
+import { useState } from 'react';
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Configure from './pages/Configuration';
+import moleratImg from './assets/molerat.jpeg';
+import BouncingBalls from './components/BouncingBalls';
 
 function App() {  
-  const [count, setCount] = useState(0)
-
-  return (
-      <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />       {/* homepage */}
-        <Route path="/about" element={<About />} />
-        <Route path="/configuration" element={<Configure />} />   {/* ✅ this line */}
-      </Routes>
-    </BrowserRouter>
-
-  )
+	return (
+	  <BrowserRouter>
+		<BouncingBalls image={moleratImg} count={15} /> {/* background balls */}
+		<Routes>
+		  <Route path="/" element={<Home />} />      
+		  <Route path="/about" element={<About />} />
+		  <Route path="/configuration" element={<Configure />} />  
+		</Routes>
+	  </BrowserRouter>
+	)
 }
+ 
 
 export default App
